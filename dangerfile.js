5277882e3c9270d3f2d4703e895bec222ddfefd2
Omri Lotan <omrilotan@users.noreply.github.com>
2019-05-06 11:39:42 +0300

    dangerouslySetInnerHTML

--- dangerfile.js
+++ dangerfile.js
@@ -1,9 +1,35 @@
-const {github: {pr: {body, base}}} = danger;
+const {
+    github: {
+        pr: {
+            body,
+            base: {
+                ref,
+            },
+        },
+    },
+    git: {
+        modified_files,
+        diffForFile,
+    },
+} = danger;
 
-if (!body || body.length < 2) {
+if (!body || body.trim().length < 2) {
     fail('Please add a description to the pull request.');
 }
 
-if (base.ref !== 'master') {
-    warn(`The base branch for this PR is \`${base.ref}\`. Are you sure you want to target something other than the \`master\` branch?`);
+if (ref !== 'master') {
+    warn(`The base branch for this PR is \`${ref}\`. Are you sure you want to target something other than the \`master\` branch?`);
 }
+
+// React dangerouslySetInnerHTML and user generated content
+(async() => {
+    let file;
+    for (file in modified_files) {
+        const diff = await diffForFile(file);
+        if (diff.includes('dangerouslySetInnerHTML')) {
+
+            // TODO: Add an informative link to UGC security concern
+            warn(`Please make sure you do not introduce and user generated content using dangerouslySetInnerHTML (${file}).`);
+        }
+    }
+})();
