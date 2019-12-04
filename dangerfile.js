73a9862c12bc970fb1ac2a26d7f9f49d5d3d504d
TomMizrachi <TomMizrachi@users.noreply.github.com>
2019-12-04 11:50:43 +0200

    Check for debugging tools

--- dangerfile.js
+++ dangerfile.js
@@ -18,6 +18,7 @@ const {
     }
 } = danger;
 
+const bindingPry = require('./src/bindingPry');
 const dangerousSetInnerHTMLWarn = require('./src/dangerousSetInnerHTMLWarn');
 const descriptionPresenceFail = require('./src/descriptionPresenceFail');
 const gemfileLockUpdateWarn = require('./src/gemfileLockUpdateWarn');
@@ -25,6 +26,7 @@ const packageLockUpdateWarn = require('./src/packageLockUpdateWarn');
 const sizeDiffWarn = require('./src/sizeDiffWarn');
 const unitTestsPresenceWarn = require('./src/unitTestsPresenceWarn');
 
+schedule(bindingPry.run(modifiedFiles, diffForFile, fail));
 schedule(dangerousSetInnerHTMLWarn.run(modifiedFiles, diffForFile, warn));
 schedule(descriptionPresenceFail.run(body, fail));
 schedule(gemfileLockUpdateWarn.run(fileMatch, warn));
---
+++ src/bindingPry/index.js
@@ -0,0 +1,45 @@
+/**
+ * Danger message warning.
+ * @constant
+ * @type {String}
+ * @default
+ */
+const MESSAGE = `<b>Performance!</b> - <i>
+"binding.pry" may cause performance hits on production environments.
+Please remove debugging tools from your code.
+</i> ⛔️`;
+
+/**
+ * Return true if detect "binding.pry" in str.
+ * @param {String} str
+ * @returns {Boolean}
+ */
+const bindingPryDetected = (str) =>
+    str && str.includes('binding.pry');
+
+/**
+ * Warn if detect "binding.pry" added.
+ * @param {String[]} files - list of modified files.
+ * @param {Function} diffForFile - danger diffForFile.
+ * @param {Function} warn - danger warn.
+ * @returns {Promise<undefined>}
+ */
+const run = async(files, diffForFile, warn) => {
+    if (!files) {
+        return;
+    }
+
+    for (const file of files) {
+        const { after } = await diffForFile(file);
+
+        if (bindingPryDetected(after)) {
+            warn(MESSAGE);
+            return;
+        }
+    }
+};
+
+module.exports = {
+    MESSAGE,
+    run
+};
