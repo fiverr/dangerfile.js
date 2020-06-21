b71ca25eb22c3330e1b3a31bbcb0cd1539d5da15
Sahar Brodbeker <saharonb@gmail.com>
2020-06-21 18:03:12 +0300

    Ignore lockfile check if it doesn't exist (#15)

--- src/packageLockUpdateWarn/index.js
+++ src/packageLockUpdateWarn/index.js
@@ -1,3 +1,6 @@
+const { join } = require('path');
+const { existsSync } = require('fs');
+
 /**
  * Danger message warning.
  * @constant
@@ -10,6 +13,16 @@ Did you forget to do \`npm i\` before commit?
 </i> 🔒`;
 
 /**
+ * The package's lockfile file name.
+ * @constant
+ * @type {String}
+ */
+const LOCKFILE_NAME = 'package-lock.json';
+
+const packageRoot = process.cwd();
+const lockfilePath = join(packageRoot, LOCKFILE_NAME);
+
+/**
  * Warn in case package.json was modified and package-lock was not.
  * @param {Function} fileMatch - danger fileMatch.
  * @param {Function} warn - danger warn.
@@ -17,7 +30,11 @@ Did you forget to do \`npm i\` before commit?
  */
 const run = async(fileMatch, warn) => {
     const packageJsonFile = fileMatch('package.json');
-    const packageLockFile = fileMatch('package-lock.json');
+    const packageLockFile = fileMatch(LOCKFILE_NAME);
+
+    if (!existsSync(lockfilePath)) {
+        return;
+    }
 
     if (packageJsonFile.modified && !packageLockFile.modified) {
         warn(MESSAGE);
