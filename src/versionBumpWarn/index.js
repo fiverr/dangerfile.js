4d88058b3ba50b7b6f0c4f97672a99738487f1a3
Or Segal <or.segal@fiverr.com>
2021-01-24 14:51:28 +0200

    newline

--- src/versionBumpWarn/index.js
+++ src/versionBumpWarn/index.js
@@ -4,8 +4,7 @@
  * @type {String}
  * @default
  */
-const MESSAGE = `<b>Version Bump!</b>
-- <i>Do you want to publish a new version of this package after merging this PR?
+const MESSAGE = `<b>Version Bump!</b> - <i>Do you want to publish a new version of this package after merging this PR?
 Don't forget to bump the version in \`package.json\` before merging to the master.
 </i> 📦`;
 
